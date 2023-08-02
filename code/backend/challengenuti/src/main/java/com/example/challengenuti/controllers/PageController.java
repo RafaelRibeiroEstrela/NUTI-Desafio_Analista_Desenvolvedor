a7b8db6dd8414e9c979cf28a816532954dc199f7
Rafael Ribeiro Estrela <74848416+RafaelRibeiroEstrela@users.noreply.github.com>
2023-08-01 22:31:47 -0400

    refactor: pages html

--- code/backend/challengenuti/src/main/java/com/example/challengenuti/controllers/PageController.java
+++ code/backend/challengenuti/src/main/java/com/example/challengenuti/controllers/PageController.java
@@ -5,14 +5,14 @@ import com.example.challengenuti.requests.PageRequest;
 import com.example.challengenuti.services.PageService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
-@RestController
-@RequestMapping("/desafio")
+@Controller
 public class PageController {
 
     private Logger LOGGER = LoggerFactory.getLogger(PageController.class);
@@ -23,19 +23,18 @@ public class PageController {
         this.pageService = pageService;
     }
 
-    @PostMapping("/identificar_tags")
-    public ResponseEntity<List<PageDTO>> save(@RequestBody PageRequest pageRequest){
-        LOGGER.info("INICIANDO save COM {}", pageRequest);
-        List<PageDTO> result = pageService.save(pageRequest);
-        LOGGER.info("FINALIZANDO save COM {}", result);
-        return ResponseEntity.status(HttpStatus.OK).body(result);
+    @GetMapping("/desafio/formulario")
+    public String form(){
+        return "form";
     }
 
-    @GetMapping("/buscar_tags_por_url")
-    public ResponseEntity<PageDTO> findByUrl(@RequestParam String url){
-        LOGGER.info("INICIANDO findByUrl COM url={}", url);
-        PageDTO result = pageService.findByUrl(url);
-        LOGGER.info("FINALIZANDO findByUrl COM {}", result);
-        return ResponseEntity.status(HttpStatus.OK).body(result);
+    @GetMapping("/desafio/identificar_tags")
+    public String save(@RequestParam List<String> urls, Model model){
+        PageRequest pageRequest = new PageRequest();
+        pageRequest.getUrls().addAll(urls);
+        List<PageDTO> result = pageService.save(pageRequest);
+        model.addAttribute("pages", result);
+        return "pages";
     }
+
 }
